413c4bbce1324edf239a893eae9b18ef4cea309d
ESLyric <102273569+ESLyric@users.noreply.github.com>
2022-03-25 00:13:59 +0800

    first commit

---
+++ searcher/qqmusic.js
@@ -0,0 +1,221 @@
+﻿
+/*
+ All credits to https://github.com/jsososo/QQMusicApi
+                https://github.com/xmcp/QRCD
+*/
+
+export function getConfig(cfg) {
+    cfg.name = "QQ音乐";
+    cfg.version = "0.1";
+    cfg.author = "ohyeah";
+    cfg.useRawMeta = false;
+}
+
+export function getLyrics(meta, man) {
+
+    evalLib("querystring/querystring.min.js");
+
+    // query QRC lyrics
+    var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_search_pc_lrc.fcg?';
+    var data = {
+        SONGNAME: meta.title,
+        SINGERNAME: meta.artist,
+        TYPE: 2,
+        RANGE_MIN: 1,
+        RANGE_MAX: 20
+    };
+    url += querystring.stringify(data);
+
+    var headers = {};
+    headers['Referer'] = 'https://y.qq.com';
+
+    settings = {
+        method: 'get',
+        url: url,
+        headers: headers
+    };
+
+    var stage_song_list = [];
+    request(settings, (err, res, body) => {
+        if (err || res.statusCode != 200) {
+            return;
+        }
+        var xml_doc = mxml.loadString(body);
+        var song_list = xml_doc.findElement('songinfo') || [];
+        for (const song of song_list) {
+            var id = song.getAttr('id');
+            if (id == null) continue;
+            var title = decodeURIComponent(getChildElementCDATA(song, 'name'));
+            var artist = decodeURIComponent(getChildElementCDATA(song, 'singername'));
+            var album = decodeURIComponent(getChildElementCDATA(song, 'albumname'));
+
+            stage_song_list.push({ id: id, title: title, artist: artist, album: album });
+        }
+
+    });
+
+    var qrcCount = 0;
+    var lyricMeta = man.createLyric();
+    for (const song of stage_song_list) {
+        url = 'https://c.y.qq.com/qqmusic/fcgi-bin/lyric_download.fcg?';
+        data = {
+            version: '15',
+            miniversion: '82',
+            lrctype: '4',
+            musicid: song.id,
+        };
+        url += querystring.stringify(data);
+
+        settings = {
+            method: 'get',
+            url: url,
+            headers: headers
+        };
+
+        request(settings, (err, res, body) => {
+            if (err || res.statusCode != 200) {
+                return;
+            }
+
+            body = body.replace('<!--', '').replace('-->', '').replace(/<miniversion.*\/>/, '').trim();
+            var xml_root = mxml.loadString(body);
+            if (xml_root != null) {
+                var lyrics = xml_root.findElement('lyric') || [];
+                for (const lyric_entry of lyrics) {
+                    var content = getChildElementCDATA(lyric_entry, 'content');
+                    if (content == null) continue;
+                    var lyricData = restoreQrc(content);
+                    if (lyricData == null) continue;
+                    lyricMeta.title = song.title;
+                    lyricMeta.artist = song.artist;
+                    lyricMeta.album = song.album;
+                    lyricMeta.lyricData = lyricData;
+                    lyricData.fileType = 'qrc';
+                    man.addLyric(lyricMeta);
+                    ++qrcCount;
+                }
+            }
+        });
+    }
+
+    // qury LRC lyrics
+    var queryNum = qrcCount > 1 ? 5 : 10;
+    url = 'http://c.y.qq.com/soso/fcgi-bin/client_search_cp?';
+    var t = 0;
+    data = {
+        format: 'json',
+        n: queryNum,
+        p: 0,
+        w: meta.title + '+' + meta.artist,
+        cr: 1,
+        g_tk: 5381
+    };
+    url += querystring.stringify(data);
+
+    var settings = {
+        method: 'get',
+        url: url,
+        headers: headers
+    };
+
+    stage_song_list = [];
+    request(settings, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+            try {
+                var obj = JSON.parse(body);
+                var data = obj['data'] || {};
+                var song = data['song'] || {};
+                var song_list = song['list'] || {};
+                for (const song_entry of song_list) {
+                    var title = song_entry['songname'] || '';
+                    var album = song_entry['albumname'] || '';
+                    var artist = '';
+                    var artist_list = song_entry['singer'] || [];
+                    if (artist_list.length > 0) {
+                        artist = artist_list[0]['name'] || '';
+                    }
+                    var songmid = song_entry['songmid'] || '';
+                    if (songmid === '') {
+                        continue;
+                    }
+                    stage_song_list.push({ title: title, album: album, artist: artist, songmid: songmid });
+                }
+            } catch (e) {
+                console.log('qqmusic exception: ' + e.message);
+            }
+        }
+    });
+
+    var lyricMeta = man.createLyric();
+    for (const result of stage_song_list) {
+        url = 'http://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg?';
+        data = {
+            songmid: result.songmid,
+            pcachetime: new Date().getTime(),
+            g_tk: 5381,
+            loginUin: 0,
+            hostUin: 0,
+            inCharset: 'utf8',
+            outCharset: 'utf-8',
+            notice: 0,
+            platform: 'yqq',
+            needNewCode: 1,
+            format: 'json'
+        };
+        url += querystring.stringify(data);
+        settings = {
+            method: 'get',
+            url: url,
+            headers: headers
+        };
+
+        request(settings, (err, res, body) => {
+            if (!err && res.statusCode === 200) {
+                lyricMeta.title = result.title;
+                lyricMeta.artist = result.artist;
+                lyricMeta.album = result.album;
+                try {
+                    var obj = JSON.parse(body);
+                    var b64lyric = obj['lyric'] || '';
+                    var b64tlyric = data['trans'] || '';
+                    var lyric = atob(b64lyric);
+                    var tlyric = atob(b64tlyric);
+                    if (tlyric != '') lyric += tlyric;
+                    lyricMeta.lyricText = lyric;
+                    man.addLyric(lyricMeta);
+                } catch (e) {
+                    console.log('qqmusic parse lyric response exception: ' + e.message);
+                }
+            }
+        });
+    }
+}
+
+function getChildElementCDATA(node, name) {
+    var child = node.findElement(name);
+    if (child == null) {
+        return '';
+    }
+    var schild = child.getFirstChild();
+    if (schild == null) {
+        return '';
+    }
+    return schild.getCDATA() || '';
+}
+
+function restoreQrc(hexText) {
+    if (hexText.length % 2 != 0) return null;
+
+    const sig = "[offset:0]\n";
+    var arrBuf = new Uint8Array(hexText.length / 2 + sig.length);
+    for (var i = 0; i < sig.length; ++i) {
+        arrBuf[i] = sig.charCodeAt(i);
+    }
+
+    const offset = sig.length;
+    for (var i = 0; i < hexText.length; i += 2) {
+        arrBuf[offset + i / 2] = parseInt(hexText.slice(i, i + 2), 16);
+    }
+
+    return arrBuf.buffer;
+}
---
+++ searcher/ttplayer_obsolete.js
@@ -0,0 +1,188 @@
+
+var SERVER_1 = "http://ttlrcct.qianqian.com";
+var SERVER_2 = "http://ttlrccnc.qianqian.com";
+
+var SERVER = SERVER_1;
+
+export function get_config(config) {
+    config.name = "千千静听(已失效)";
+    config.version = "0.3";
+    config.author = "ohyeah";
+    config.useRawMeta = false;
+}
+
+export function get_lyrics(meta, man) {
+    var title = meta.title;
+    var artist = meta.artist;
+    var lyrics_arr = [];
+    request(createReqUrl(meta.artist, meta.title),
+        (err, res, body) => {
+            if (!err & res.statusCode == 200) {
+                var xml_doc = mxml.loadString(body);
+                if (!xml_doc) return;
+                var lyric_nodes = xml_doc.findElement("lrc");
+                if (!lyric_nodes) return;
+                for (const node of lyric_nodes) {
+                    lyrics_arr.push({
+                        id: node.getAttr("id"),
+                        artist: node.getAttr("artist"),
+                        title: node.getAttr("title")
+                    });
+                }
+            }
+        });
+    var lyric_meta = man.createLyric();
+    for (const node of lyrics_arr) {
+        request(createReqUrl(node.artist, node.title, node.id),
+            (err, res, body) => {
+                if (!err & res.statusCode == 200) {
+                    lyric_meta.title = node.title;
+                    lyric_meta.artist = node.artist;
+                    lyric_meta.lyricText = body;
+                    man.addLyric(lyric_meta);
+                }
+            });
+    }
+}
+
+function createReqUrl(artist, title, id) {
+    var url;
+    if (id) {
+        var code = calcCode(artist, title, id);
+        url = SERVER + "/dll/lyricsvr.dll?dl?Id=" + id + "&Code=" + code.toString(10);
+    } else {
+        title = procKeywords(title);
+        artist = procKeywords(artist);
+        url = SERVER + "/dll/lyricsvr.dll?sh?Artist=" + dumpHexUnicodeLE(artist) + "&Title=" + dumpHexUnicodeLE(title) + "&Flags=0";
+    }
+    return url;
+}
+
+function procKeywords(str) {
+    var s = str;
+    s = s.toLowerCase();
+    s = s.replace(/\'|·|\$|\&|–/g, "");
+    //trim all spaces
+    s = s.replace(/(\s*)|(\s*$)/g, "");
+    //truncate all symbols
+    s = s.replace(/\(.*?\)|\[.*?]|{.*?}|（.*?/g, "");
+    s = s.replace(/[-/:-@[-`{-~]+/g, "");
+    s = s.replace(/[\u2014\u2018\u201c\u2026\u3001\u3002\u300a\u300b\u300e\u300f\u3010\u3011\u30fb\uff01\uff08\uff09\uff0c\uff1a\uff1b\uff1f\uff5e\uffe5]+/g, "");
+    return s;
+}
+
+function mult32s(n, m) //signed version
+{
+    n |= 0; m |= 0;
+    var nlo = n & 0xffff;
+    var nhi = n - nlo;
+    return ((nhi * m | 0) + (nlo * m)) | 0;
+}
+
+function calcCode(artist, title, id) {
+    var info = artist + title;
+    var utf8hex = dumpHexUTF8(info);
+    var code = [];
+    var len = utf8hex.length / 2;
+    for (var i = 0; i < utf8hex.length; i += 2) {
+        code[i / 2] = parseInt(utf8hex.substr(i, 2), 16);
+    }
+    var t1 = 0,
+        t2 = 0,
+        t3 = 0;
+    t1 = (id & 0x0000FF00) >> 8;
+    if ((id & 0x00FF0000) == 0) t3 = 0x000000FF & ~t1;
+    else t3 = 0x000000FF & ((id & 0x00FF0000) >> 16);
+    t3 = t3 | ((0x000000FF & id) << 8);
+    t3 = t3 << 8;
+    t3 = t3 | (0x000000FF & t1);
+    t3 = t3 << 8;
+    if ((id & 0xFF000000) == 0) t3 = t3 | (0x000000FF & (~id));
+    else t3 = t3 | (0x000000FF & (id >> 24));
+
+    var j = len - 1;
+    while (j >= 0) {
+        var c = code[j];
+        if (c >= 0x80) c = c - 0x100;
+        t1 = ((c + t2) & 0xFFFFFFFF);
+        t2 = ((t2 << (j % 2 + 4)) & 0xFFFFFFFF);
+        t2 = ((t1 + t2) & 0xFFFFFFFF);
+        j--;
+    }
+    j = 0;
+    t1 = 0;
+    while (j < len) {
+        var c = code[j];
+        if (c >= 128) c = c - 256;
+        var t4 = ((c + t1) & 0xFFFFFFFF);
+        t1 = ((t1 << (j % 2 + 3)) & 0xFFFFFFFF);
+        t1 = ((t1 + t4) & 0xFFFFFFFF);
+        j++;
+    }
+    var t5 = conv(t2 ^ t3);
+    t5 = conv(t5 + (t1 | id));
+    t5 = conv(mult32s(t5, (t1 | t3)));
+    t5 = conv(mult32s(t5, (t2 ^ id)));
+    if (t5 > 0x80000000) t5 = (t5 - 0x100000000) & 0xFFFFFFFF;
+    return t5;
+}
+
+//Unicode | UTF-8
+//(HEX)   | (BIN)
+//--------------------+---------------------------------------------
+//0000 0000-0000 007F | 0xxxxxxx
+//0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+//0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+//0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+function dumpHexUTF8(str) {
+    var ret = "";
+
+    for (var i = 0; i < str.length; i++) {
+        var c = str.charCodeAt(i);
+        var b = 0;
+        if (c < 0x0080) { // 0000 - 007F
+            b = c & 0x000000ff;
+        } else if (c < 0x800) { // 0080 - 07FF
+            b = (0xC0 | ((c & 0x7C0) >> 6)) << 8;
+            b |= (0x80 | (c & 0x3F)) << 0;
+        } else if (c < 0x010000) { // 0800 - FFFF
+            b = (0xE0 | ((c & 0xF000) >> 12)) << 16;
+            b |= (0x80 | ((c & 0xFC0) >> 6)) << 8;
+            b |= (0x80 | (c & 0x3F)) << 0;
+        } else { // 0x010000 - 
+            b = (0xF0 | ((c & 0x1C0000) >> 18)) << 24;
+            b |= (0x80 | ((c & 0x3F000) >> 12)) << 16;
+            b |= (0x80 | ((c & 0xFC0) >> 6)) << 8;
+            b |= (0x80 | (c & 0x3F)) << 0;
+        }
+        ret += b.toString(16).toUpperCase();
+    }
+
+    return ret;
+}
+
+function dumpHexUnicodeLE(str) {
+    var ret = "";
+    for (var i = 0; i < str.length; i++) {
+        var b = str.charCodeAt(i);
+        var bs = "";
+        //convert UNICODE BE to LE
+        var lb = (b & 0xff00) >> 8;
+        var hb = (b & 0x00ff) >> 0;
+        if (hb < 0x10) bs += "0";
+        bs += hb.toString(16).toUpperCase();
+        if (lb < 0x10) bs += "0";
+        bs += lb.toString(16).toUpperCase();
+
+        ret += bs;
+    }
+    return ret;
+}
+
+function conv(i) {
+    i &= 0xFFFFFFFF;
+    var r = i % 0x100000000;
+    if (i >= 0 && r > 0x80000000) r = r - 0x100000000;
+    if (i < 0 && r < 0x80000000) r = r + 0x100000000;
+    return r & 0xFFFFFFFF;
+}
